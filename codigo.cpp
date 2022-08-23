eecd03eff5338bdaf7cdb1208e094db97632f922
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-08-23 17:16:02 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -245,4 +245,45 @@ int busca_binaria(int vet[], int procurado, int inicio, int fim) //faz uma busca
        return busca_binaria(vet, procurado,inicio , ((inicio+fim)/2));
 }
 
-    
+void marge_vet(int vet[], int inicio, int fim) //faz o marge sort
+{
+    int meio=(inicio+fim)/2, aux[(fim-inicio)+1];
+    for (int i=inicio, j=meio+1, k=0; i<=meio || j<=fim;)
+    {
+        if (i<=meio && j<=fim)
+        {
+            if (vet[i]<vet[j])
+            {
+                aux[k]=vet[i];
+                k++;
+                i++;
+            }
+            else
+            {
+                aux[k]=vet[j];
+                k++;
+                j++;
+            }
+        }
+        else
+        {
+            if (i>meio)
+            {
+                aux[k]=vet[j];
+                k++;
+                j++;
+            }
+            else
+            {
+                aux[k]=vet[i];
+                k++;
+                i++;
+            }
+        }
+    }
+
+    for (int i=0, j=inicio;i<(fim-inicio)+1; j++, i++)
+    {
+        vet[j]=aux[i];
+    }
+}
