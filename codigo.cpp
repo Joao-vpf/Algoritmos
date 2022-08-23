433217c90aa2d132b734df7ef666a4438b0ef6b8
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-08-23 17:07:20 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -234,17 +234,15 @@ int fat(int n) //cria um fatorial
 
 int busca_binaria(int vet[], int procurado, int inicio, int fim) //faz uma busca binaria
 {
-    cout << inicio << " " << fim << endl;
     if (inicio==fim)
     {
         return (vet[inicio]==procurado ? inicio : -1);
     }
     if (vet[(inicio+fim)/2]<=procurado)
     {
-        return busca_binaria(vet, procurado, ((inicio+fim)/2)+1, fim);
+       return busca_binaria(vet, procurado, ((inicio+fim)/2)+1, fim);
     }
-        return busca_binaria(vet, procurado,inicio , ((inicio+fim)/2));
-
+       return busca_binaria(vet, procurado,inicio , ((inicio+fim)/2));
 }
 
     
