d86f407454dc8da053eabe585a1e2ea0fa5b4f63
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-09-03 10:17:40 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -62,6 +62,27 @@ void bfs(int u) //bfs analisa por adj
     }
 }
 
+
+void bfs(int x, int pes) //peso diminui encontra limite
+{
+	dist[x]=pes;
+	queue <int> q;
+	q.push(x);
+	while (!q.empty())
+	{
+		int u= q.front();
+		q.pop();
+		for (auto e : adj[u])
+		{
+			if (dist[e]==-1 && dist[u]>0)
+			{
+				dist[e]=dist[u]-1;
+				q.push(e);
+			}
+		}
+	}
+}
+
 bool bicoloring(int x)//Objetivo identificar um grafo impar
 {
 	queue <int> q;
