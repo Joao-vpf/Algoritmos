0228b0e0ef69beee7725f87e48ea154ff620863e
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-09-24 07:36:36 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -374,6 +374,27 @@ int contador (int v) //conta quantos nodos estão conexos funciona melhor com na
 
 //Funções: 
 
+int multiplicar_matrizes( //calcula multiplicação de matriz de mesmo tamanho
+        int matA[][MAX], int na, int ma,
+        int matB[][MAX], int nb, int mb,
+        int matC[][MAX], int nc, int mc)
+{  
+
+    if (ma != nb || na != nc || mb != mc) return 1;
+
+    int a = na, b = ma, c = mb;
+
+    for (int i = 0; i < a; i++) {
+        for (int k = 0; k < c; k++) {
+            matC[i][k] = 0;
+            for (int j = 0; j < b; j++) {
+                matC[i][k] += matA[i][j] * matB[j][k];
+            }
+        }
+    }
+    return 0;
+}
+
 int trad (char x) //pega uma letra qualquer e diz qual posicao de 0 a 25 ela pertence
 {
     int o=x-97;
