bc263a5aa0535909df5a9d461956950055401071
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-09-24 07:56:01 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -374,6 +374,14 @@ int contador (int v) //conta quantos nodos estão conexos funciona melhor com na
 
 //Funções: 
 
+int mdc(int num1, int num2) //maior e menor
+{
+    if(num1%num2 == 0)
+        return num2;
+    else
+        return mdc(num2, num1%num2);
+}
+
 int mmc(int num1, int num2) { //mmc
 
     int resto, a, b;
