804c89859128f39e70e48732f6e5760d41f97775
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-10-13 15:45:07 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -96,6 +96,55 @@ void bfs(int x, int pes) //peso diminui encontra limite
 		}
 	}
 }
+//PINTADORES DE GRAFO 
+/*
+ Esta função resolve a coloração m
+problema com a utilização da recursividade. Retorna
+falso se as cores m não puderem ser atribuídas,
+caso contrário, devolver verdadeiro e impressões
+atribuições de cores a todos os vértices.
+É favor notar que pode haver mais de
+uma solução, esta função imprime uma
+das soluções viáveis.
+*/
+bool isSafe(bool graph[V][V], int color[]) //olha se esse ponto foi pintado
+{
+    // check for every edge
+    for (int i = 0; i < V; i++)
+        for (int j = i + 1; j < V; j++)
+            if (graph[i][j] && color[j] == color[i])
+                return false;
+    return true;
+}
+
+bool graphColoring(bool graph[V][V], int m, int i, int color[V]) //procura se da para pintar com esse numero de nodo
+{
+    // if current index reached end
+    if (i == V) {
+ 
+        // if coloring is safe
+        if (isSafe(graph, color)) {
+ 
+            // Print the solution
+            printSolution(color);
+            return true;
+        }
+        return false;
+    }
+ 
+    // Assign each color from 1 to m
+    for (int j = 1; j <= m; j++) {
+        color[i] = j;
+ 
+        // Recur of the rest vertices
+        if (graphColoring(graph, m, i + 1, color))
+            return true;
+ 
+        color[i] = 0;
+    }
+ 
+    return false;
+}
 
 bool bicoloring(int x)//Objetivo identificar um grafo impar
 {
