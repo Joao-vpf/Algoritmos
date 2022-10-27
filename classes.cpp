eeabc3dea358f9d4a7279f6badae9242a2b8c830
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-10-27 16:08:57 -0300

    Fenwick

--- classes.cpp
+++ classes.cpp
@@ -89,3 +89,56 @@ class alltopologic
     }
 
 };
+
+
+
+class fenwick_tree
+{
+	private:
+	int n;
+	vector<int> jt;
+	int query(int id)
+	{
+		int res = 0;
+		for (;id; id = id - (id& -id)) // compara em bytes e acha o primeiro numero da direita para a esquerda
+		{
+			res+=jt[id];
+		} 
+		return res;
+		
+	}
+	
+	void upd(int id, int x)
+	{
+		for (; id<=n; id=id+(id&-id))
+		{
+			jt[id]+=x;
+		}
+	}
+	
+	public:
+	
+	fenwick_tree(int nl)
+	{
+		n=nl+1;
+		jt.assign(n,0);
+	}
+	
+	int query(int l , int r)
+	{
+		return query(r) - (l?query(l-1):0);
+	}
+	
+	void up(int id, int x)
+	{
+		upd(id, x);
+	}
+	
+	void upd(int l, int r, int x)
+	{
+		upd(l,x);
+		upd(x,l);
+		up(r+1, x);
+		up(r, -x);
+	}
+};
