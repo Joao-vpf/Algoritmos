554af68bca8f6291714a8efb7aa8f3662bbb770f
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-10-29 11:51:41 -0300

    bellman ford com stl

--- codigo.cpp
+++ codigo.cpp
@@ -534,33 +534,45 @@ int back(int i, int j) //bfs flood fill[
 }
 
 //Bellman Ford	
-	
-bool bford(int V, int E)
+void dfsbelam(int x, vector<int> caca[], vector<bool>& vis) //pinta todos que estao em ciclos
 {
-    vector <int> dis(V, INT_MAX);
-    dis[0] = 0;
-
-    for (int i = 0; i < V - 1; i++) {
-
-        for (int j = 0; j < E; j++) {
-            if (dis[adj[j][0]] != INT_MAX && dis[adj[j][1]]>dis[adj[j][0]] + adj[j][2])
-                dis[adj[j][1]] =dis[adj[j][0]] + adj[j][2];
-        }
-    }
-
-    for (int i = 0; i < E; i++) {
-        int x = adj[i][0];
-        int y = adj[i][1];
-        int weight = adj[i][2];
-        if (dis[x] != INT_MAX && dis[x] + weight < dis[y]) //verifica se tem um ciclo negativo
+    vis[x]=true;
+    for (auto e: caca[x])
+    {
+        if(!vis[e])
         {
-            return true;
+            dfsbelam(e, caca,vis);
         }
     }
-    return false;
-/*
+}
+
+void bford(int v, int E,vector<pair<int,pair<int,int>>>& adj, vector<int>& dist, vector<bool>& vis, vector <int> caca[])
+{
+    dist[0]=0;
+    for (int i=0; i<v-1; i++)
+    {
+        for (int j=0; j<E; j++) 
+	{
+            pair<int, pair<int, int>> e = adj[j];
+            pair<int, int> l = e.second;
+            if (dist[e.first] != INT_MAX && dist[l.first] > l.second + dist[e.first]) {
+                dist[l.first] = l.second + dist[e.first];
+            }
+        }
+    }
+    for (int i = 0; i < E; i++) {
+        pair<int, pair<int, int>> e = adj[i];
+        pair<int, int> l = e.second;
+        if (dist[e.first] != INT_MAX && dist[e.first] + l.second < dist[l.first]) //verifica se tem um ciclo negativo caso tenha ele  entra no if
+        {
+           dfsbelman(e.first, caca, vis);
+        }
+    }
+	
     for (int i = 0; i < V; i++) //mostra a distancia de cada um
-        cout << i << "\t\t" << dis[i] << endl;*/
+    {
+        cout << i << "\t\t" << dis[i] << endl;
+    }
 }
 
 	
