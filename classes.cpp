2be107d5ea16e06c931c89794d888e93795c0f0d
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-11-03 15:09:25 -0300

    segtree

--- classes.cpp
+++ classes.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+const int N = 100000;
+int tree[2 * N];
+struct segtree
+{
+    int n;
+    void build(vector<int> arr) 
+    {
+        for (int i = 0; i < n; i++)
+            tree[n + i] = arr[i];
+
+        for (int i = n - 1; i > 0; --i)
+            tree[i] = tree[i << 1] + tree[i << 1 | 1];
+    }
+
+    void update(int p, int value) 
+    {
+        tree[p + n] = value;
+        p = p + n;
+
+        for (int i = p; i > 1; i >>= 1)
+            tree[i >> 1] = tree[i] + tree[i ^ 1];
+    }
+
+    int query(int l, int r) 
+    {
+        int res = 0;
+
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) 
+        {
+            if (l & 1)
+                res += tree[l++];
+
+            if (r & 1)
+                res += tree[--r];
+        }
+
+        return res;
+    }
+}
+
 class alltopologic
 {
     public:
