5874474e77558ca3136818298937f7fe7e96608b
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-11-30 16:04:43 -0300

    maior divisor primo

--- math.cpp
+++ math.cpp
@@ -141,3 +141,33 @@ vector<int> primeFactors(long long n, vector<int>& factors) {
     if (n != 1) factors.push_back(n); // remaining num is a prime
     return factors;
 }
+
+//----------Maior divisor prim-------------------
+long long maxPrimeFactors(long long n)
+{
+    long long maxPrime = -1;
+    while (n % 2 == 0) {
+        maxPrime = 2;
+        n >>= 1; 
+    }
+     while (n % 3 == 0) {
+        maxPrime = 3;
+        n=n/3;
+    }
+ 
+    for (int i = 5; i <= sqrt(n); i += 6) {
+        while (n % i == 0) {
+            maxPrime = i;
+            n = n / i;
+        }
+      while (n % (i+2) == 0) {
+            maxPrime = i+2;
+            n = n / (i+2);
+        }
+    }
+ 
+    if (n > 4)
+        maxPrime = n;
+ 
+    return maxPrime;
+}
