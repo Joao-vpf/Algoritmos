bb86e60eef40825b3cccf35bc8d3d820f1ad318c
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2022-11-30 12:09:01 -0300

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -1,3 +1,5 @@
+//Arvore de fatores de um numero
+
 // Tree node
 struct Node
 {
@@ -62,3 +64,42 @@ void printLevelOrder(Node *root)
 
 createFactorTree(&root, val);//cria o node a partir do valor que voce quer
 printLevelOrder(root); //mostra os divisores
+
+//Acha a altura da arvore consequentimente a quantidade de fatores
+// Function to find the height of the
+// Factor Tree of the integer N
+int factorTree(int N)
+{
+    // Stores the height of Factor Tree
+    int height = 0;
+ 
+    // Loop to iterate over values of N
+    while (N > 1) {
+        // Stores if there exist
+        // a factor of N or not
+        bool flag = false;
+ 
+        // Loop to find the smallest
+        // factor of N
+        for (int i = 2; i <= sqrt(N); i++) {
+            // If i is a factor of N
+            if (N % i == 0) {
+                N = N / i;
+                flag = true;
+                break;
+            }
+        }
+ 
+        // Increment the height
+        height++;
+ 
+        // If there are no factors of N
+        // i.e, N is prime, break loop
+        if (!flag) {
+            break;
+        }
+    }
+ 
+    // Return Answer
+    return height;
+}
