01c29911550d3c8e0c517445ef2445950101d334
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2023-01-03 10:57:50 -0300

    Update dfs.cpp

--- Grafos/dfs.cpp
+++ Grafos/dfs.cpp
@@ -10,7 +10,7 @@ void dfs(int x)
     }
 }
 
-
+//VARIAÇÕES
 void dfs(int u) //dfs que analisa por adj
 {
     queue <int> q;
@@ -25,4 +25,36 @@ void dfs(int u) //dfs que analisa por adj
             q.push(e);
         }
     }
+}
+
+void dfsStack(int u) //dfs stack (confere por adjacência)
+{
+    stack<int> s;
+    s.push(u);
+    while (!s.empty())
+    {
+        int v = s.top();
+        s.pop();
+        vis[v] = true;
+        for (auto e : adj[v])
+        {
+            if (!vis[e])
+            {
+                s.push(e);
+            }
+        }
+    }
+}
+
+void dfs(int u, int j) //dfs conta o tempo de saida e de entrada;
+{
+    temp[u].first=timer++;
+    for (int v :adj[u])
+    {
+        if(v!=j)
+        {
+            dfs(v,u);
+        }
+    }
+    temp[v].second=timer++;
 }
