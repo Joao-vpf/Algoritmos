f6b8a2e4385f02b5d44b0ebe2ad5043f815a7f6e
Joao-vpf <98399932+Joao-vpf@users.noreply.github.com>
2023-01-04 10:20:13 -0300

    soma range

--- pd/range.cpp
+++ pd/range.cpp
@@ -1,4 +1,4 @@
-maxrange(vector<long long> a)
+long long produto_maxrange(vector<long long> a)
 {
   long long maxatual = a[0];
   long long minatual = a[0];
@@ -11,4 +11,23 @@ maxrange(vector<long long> a)
       maior = max(maior, maxatual);
   }
   return maior
+}
+
+int soma_maxrange(vector<int> a)
+{
+    int maior = INT_MIN, atual = 0;
+    for (int i = 0; i < n; i++) 
+    {
+       atual += a[i];
+        if (maior <atual)
+        {
+            maior = atual;
+        }
+
+        if (atual < 0)
+        {
+            atual = 0;
+        }
+    }
+  return maior;
 }
