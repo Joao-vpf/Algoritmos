25740409ee80145278a6c770d003204025e1e64e
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-02-01 17:00:16 -0300

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -1224,3 +1224,20 @@ void getMaxOccuringElement(int arr[], int N, int M,pair<int, int> Q[])
 	}
 }
 
+*/
+//O(n*N!);
+void permute(string& a, int l, int r)
+{
+    if (l == r)
+    {
+       cout << a << endl;
+    }
+    else {
+        for (int i = l; i <= r; i++) {
+ 
+            swap(a[l], a[i]);
+            permute(a, l + 1, r);
+            swap(a[l], a[i]);
+        }
+    }
+}
