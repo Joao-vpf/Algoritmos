0a3cb9340b8c0e2be283958d22f27798fe9b7e82
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-02-14 15:11:31 -0300

    Update math.cpp

--- math.cpp
+++ math.cpp
@@ -784,3 +784,23 @@ ll primeFactors(int n) //conta os fatores de um numero O(Sqrt(n))
 
     return cont;
 } 
+
+//fibonnaci log(n)
+ull f[1000] = {0};
+
+ull fib(ull n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1 || n == 2)
+        return (f[n] = 1);
+ 
+    if (f[n])
+        return f[n];
+ 
+    ull k = (n & 1)? (n+1)/2 : n/2;
+
+    f[n] = (n & 1)? (fib(k)*fib(k) + fib(k-1)*fib(k-1)): (2*fib(k-1) + fib(k))*fib(k);
+ 
+    return f[n];
+}
