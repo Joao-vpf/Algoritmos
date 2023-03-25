547d24129f77b7ebc1b3804efd6385f20a8fff6a
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-03-25 10:59:45 -0300

    Update Lis(string-retorna quantidade).cpp

--- string/Lis(string-retorna quantidade).cpp
+++ string/Lis(string-retorna quantidade).cpp
@@ -1,11 +1,14 @@
-int lis(string s) {
+int lis(string s) 
+{//O(nlogn)
     int n = s.size();
     vector<int> dp(n, 1); // vetor dp com valor 1 em todas as posições
     int res = 1; // valor máximo inicializado como 1
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) 
+    {
         // busca binária pelo maior valor menor que dp[i]
         int lo = 0, hi = res-1, j = -1;
-        while (lo <= hi) {
+        while (lo <= hi)
+        {
             int mid = (lo + hi) / 2;
             if (s[i] > s[j = mid]) lo = mid + 1;
             else hi = mid - 1;
