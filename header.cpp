28221d107aebe04d6dbde7cfb151ea293d1601a8
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-04-19 15:03:55 -0300

    Create header.cpp

---
+++ header.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define fr first
+#define sd second
+#define endl "\n"
+#define freop freopen("a.txt","r", stdin);freopen("11.txt","w", stdout); 
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define MAX INT_MAX
+#define unormap unordered_map 
+#define FOR(i,a,b) for (int i=(a); i<(b); i++)
+#define FOR(i,a,b) for (int i=(a); i<(b); i++)
+#define sort(a) sort(a.begin(), a.end())
+#define vecprnt(v, n) for(int i=0; i<n; i++) cout<<v[i] <<endl 
+using namespace std;
+typedef long int li;
+typedef long long ll;
+typedef unsigned long long ull;
+
+/* 
+Direção array
+
+int fx[]={1,-1,0,0};
+int fy[]={0,0,1,-1};
+int fx[]={0,0,1,-1,-1,1,-1,1};
+int fy[]={-1,1,0,0,1,1,-1,-1};
+
+*/
+
