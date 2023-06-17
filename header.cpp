44560ea64e0eff6a8fdeca7827b5b193540c08d2
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-06-17 08:01:23 -0300

    Update header.cpp

--- header.cpp
+++ header.cpp
@@ -1,3 +1,17 @@
+/*
+Portin esteve aqui
+⠀⠀⠀⠀⠱⣶⣶⣶⣶⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣤⣵⣶⣾⣷⣀⠈⠛⢿⣷⡄⠀⠀⠀⣠
+⠀⠀⢀⣾⠟⠋⣭⣯⠙⠿⠸⣿⣿⠎⣿⣿⣎⠿⣿⡿
+⠀⢰⣿⠏⠀⠀⣿⣿⠀⠀⠀⣿⣿⠀⣿⣿⠋⠀⠙⠁
+⣤⣿⣿⠀⠀⠀⣿⣿⠀⠀⠀⣿⣿⢀⣿⠏⠀⠀⠀⠀
+⠸⣿⣿⡀⠀⠀⣿⣿⣶⣦⣤⣛⢿⣾⣷⣶⣶⣾⠀⠀
+⠀⠙⣿⣷⡀⠀⣿⣿⠀⠈⠙⢿⣷⡄⠀⠈⢻⠏⠀⠀
+⠀⠀⠈⠛⢿⣷⣯⣭⣤⣤⡆⣷⡝⣿⣆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣯⣭⠉⠉⢀⣿⣿⠈⢿⣷⣤⣤⡴⠀
+⠀⠀⠀⠀⠀⠰⢿⣿⣆⠀⠼⣿⣿⡄⠈⠻⠿⠟⠁
+ 
+*/
 #include <bits/stdc++.h>
 #define pb push_back
 #define pf push_front
@@ -10,22 +24,40 @@
 #define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define MAX INT_MAX
 #define unormap unordered_map 
-#define FOR(i,a,b) for (int i=(a); i<(b); i++)
-#define FOR(i,a,b) for (int i=(a); i<(b); i++)
-#define sort(a) sort(a.begin(), a.end())
+//#define sort(a) sort(a.begin(), a.end())
 #define vecprnt(v, n) for(int i=0; i<n; i++) cout<<v[i] <<endl 
+#define upb upper_bound
+#define pii pair<int,int>
 using namespace std;
 typedef long int li;
 typedef long long ll;
 typedef unsigned long long ull;
 
-/* 
-Direção array
-
 int fx[]={1,-1,0,0};
 int fy[]={0,0,1,-1};
+/*
 int fx[]={0,0,1,-1,-1,1,-1,1};
 int fy[]={-1,1,0,0,1,1,-1,-1};
-
 */
+
+int main()
+{
+	ios
+	//freop
+	
+}
+
+
+/*
+int main()
+{
+	int n = rand()%1000;
+	cout << n <<endl;
+	for(int i=0; i<n; i++)
+	{
+		cout << rand%100 << " ";
+	}
+	cout<<endl;
+}
+*/ 
 
