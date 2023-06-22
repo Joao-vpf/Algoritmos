4980fa3882ea35f08b53f7db5bbab4c53b1aac57
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-06-22 11:50:34 -0300

    Update fastpow.cpp

--- math/fastpow.cpp
+++ math/fastpow.cpp
@@ -17,3 +17,23 @@ T fastPow(T a, T p) {
 
     return res;
 }
+
+// Complexidade: O(log p)
+
+template <typename T>
+T fastPow(T a, T p) {
+    if (p == 0)
+        return 1;
+
+    T res = 1;
+
+    while (p > 0) {
+        if (p & 1) // Se o bit menos significativo de p é 1
+            res *= a;
+
+        a *= a;
+        p >>= 1; // Desloca p para a direita, removendo o bit menos significativo
+    }
+
+    return res;
+}
