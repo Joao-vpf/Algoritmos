0e1bd6297126d96831941e4cbbe17f02d9ff343e
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-06-22 11:47:27 -0300

    Create fastpow.cpp

---
+++ math/fastpow.cpp
@@ -0,0 +1,18 @@
+// Complexidade: O(log p)
+
+long long fastPow(long long a, long long p) {
+    if (p == 0)
+        return 1;
+
+    long long res = 1;
+
+    while (p > 0) {
+        if (p & 1) // Se o bit menos significativo de p é 1
+            res *= a;
+
+        a *= a;
+        p >>= 1; // Desloca p para a direita, removendo o bit menos significativo
+    }
+
+    return res;
+}
