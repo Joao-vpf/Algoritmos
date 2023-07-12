f8a0515f203f8da8ac3d2e80613fe613a06de106
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-07-12 15:11:09 -0300

    Update Rolling Hash O(n log n).cpp

--- string/Rolling Hash O(n log n).cpp
+++ string/Rolling Hash O(n log n).cpp
@@ -1,82 +1,67 @@
-const ll PRIME = 31;
-const ll MOD = 1e9 + 9;
+const int BASE = 31;
+const int MOD = 1e9 + 7;
 
-ll power(ll a, ll b, ll m) {
-    ll res = 1;
-    while (b > 0) {
-        if (b & 1) {
-            res = (res * a) % m;
-        }
-        a = (a * a) % m;
-        b >>= 1;
-    }
-    return res;
+vector<int> computePowers(int n) {
+    vector<int> powers(n);
+    powers[0] = 1;
+    for (int i = 1; i < n; i++)
+        powers[i] = (powers[i - 1] * 1LL * BASE) % MOD;
+    return powers;
 }
 
-ll calculateHash(const string& str) {
-    ll hashValue = 0;
-    ll p = 1;
-    for (char c : str) {
-        hashValue = (hashValue + (c - 'a' + 1) * p) % MOD;
-        p = (p * PRIME) % MOD;
-    }
-    return hashValue;
-}
-
-vector<int> getRollingHashes(const string& text, int patternLength) {
-    int n = text.length();
-    vector<int> hashes(n - patternLength + 1);
-
-    ll currHash = calculateHash(text.substr(n - patternLength, patternLength));
-    hashes[n - patternLength] = currHash;
-
-    ll powerValue = power(PRIME, patternLength - 1, MOD);
-
-    for (int i = n - patternLength - 1; i >= 0; i--) {
-        currHash = (currHash - (text[i + patternLength] - 'a' + 1) * powerValue) % MOD;
-        currHash = (currHash * PRIME) % MOD;
-        currHash = (currHash + (text[i] - 'a' + 1) + MOD) % MOD;
-        hashes[i] = currHash;
-    }
-
+vector<int> computeHashes(const string& s) {
+    int n = s.length();
+    vector<int> hashes(n + 1);
+    for (int i = 0; i < n; i++)
+        hashes[i + 1] = (hashes[i] * 1LL * BASE + s[i] - 'a' + 1) % MOD;
     return hashes;
 }
 
-int longestCommonSubsequence(const string& text1, const string& text2) {
-    int m = text1.length();
-    int n = text2.length();
-    
-    // Gerar Rolling Hashes dos dois textos
-    vector<int> hashes1 = getRollingHashes(text1, m);
-    vector<int> hashes2 = getRollingHashes(text2, n);
+unordered_set<int> computeRollingHashes(const string& s, const vector<int>& powers, int len) {
+    int n = s.length();
+    unordered_set<int> hashes;
+    int curr_hash = 0;
+    for (int i = 0; i < len; i++)
+        curr_hash = (curr_hash * 1LL * BASE + s[i] - 'a' + 1) % MOD;
+    hashes.insert(curr_hash);
+    for (int i = len; i < n; i++) {
+        curr_hash = (curr_hash - (s[i - len] - 'a' + 1) * 1LL * powers[len - 1]) % MOD;
+        curr_hash = (curr_hash * 1LL * BASE + s[i] - 'a' + 1) % MOD;
+        curr_hash = (curr_hash + MOD) % MOD;
+        hashes.insert(curr_hash);
+    }
+    return hashes;
+}
 
-    // Converter hashes2 em um conjunto para verificação rápida de colisões
-    unordered_set<int> hashSet(hashes2.begin(), hashes2.end());
+int findCommonLength(const string& s1, const string& s2, int len) {
+    int n1 = s1.length();
+    int n2 = s2.length();
 
-    int maxLength = 0;
+    vector<int> powers = computePowers(max(n1, n2) + 1);
+    unordered_set<int> hashes1 = computeRollingHashes(s1, powers, len);
+    unordered_set<int> hashes2 = computeRollingHashes(s2, powers, len);
 
-    // Verificar colisões dos hashes1 com os hashes2
-    for (int i = 0; i < hashes1.size(); i++) {
-        if (hashSet.find(hashes1[i]) != hashSet.end()) {
-            // Encontrado um possível match
-            int length = 0;
-            int idx1 = i;
-            int idx2 = 0;
-
-            // Expandir o match
-            while (idx1 < m && idx2 < n) {
-                if (text1[idx1] == text2[idx2]) {
-                    length++;
-                    idx1++;
-                    idx2++;
-                } else {
-                    break;
-                }
-            }
-
-            maxLength = max(maxLength, length);
-        }
+    for (int hash1 : hashes1) {
+        if (hashes2.count(hash1))
+            return len;
     }
 
-    return maxLength;
+    return 0;
+}
+
+int LCS(const string& s1, const string& s2) {
+    int n1 = s1.length();
+    int n2 = s2.length();
+    int left = 0;
+    int right = min(n1, n2) + 1;
+
+    while (left < right - 1) {
+        int mid = (left + right) / 2;
+        if (findCommonLength(s1, s2, mid))
+            left = mid;
+        else
+            right = mid;
+    }
+
+    return left;
 }
