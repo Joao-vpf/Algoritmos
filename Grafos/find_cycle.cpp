34cb3fc0aaccfa3a888f080b3e6d6da3afc0ba43
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-07-22 10:11:46 -0300

    Update find_cycle.cpp

--- Grafos/find_cycle.cpp
+++ Grafos/find_cycle.cpp
@@ -1,37 +1,31 @@
-const int MAXN = 100005; // número máximo de vértices
 
-vector<int> adj[MAXN]; // lista de adjacência
-bool visited[MAXN]; // vetor de visitados
-int entryTime[MAXN], exitTime[MAXN]; // tempo de entrada e saída de cada vértice
-int t = 0; // contador de tempo
-stack<int> s; // pilha para manter os vértices do caminho atual
-bool ok=1;//0 houve ciclo 1 nao teve ciclo
-void dfs(int u) 
+
+const int MAX_N = 2e5 + 5;
+
+vector<int> adj[MAX_N];
+bool vis[MAX_N], st[MAX_N];
+vector<int> cycle;
+
+bool dfs(int v) 
 {
-    visited[u] = true;
-    entryTime[u] = ++t;
-    s.push(u); // adiciona u à pilha
+    vis[v] = true;
+    st[v] = true;
+    cycle.push_back(v);
 
-    for(int v : adj[u]) 
+    for (int u : adj[v])
     {
-        if(!visited[v]) 
+        if (!vis[u]) 
         {
-            dfs(v);
-        }
-        else if(exitTime[v] == 0) 
-        {    
-            cout << "Ciclo encontrado: ";
-            while(s.top() != v) 
-            { // remove os vértices do caminho até encontrar v
-                cout << s.top() << " ";
-                s.pop();
-            }
-            cout << s.top() << endl; // imprime o vértice v que forma o ciclo
-            s.pop();
-           ok=0;
+            if (dfs(u))
+                return true;
+        } else if (st[u]) 
+        {
+            cycle.push_back(u);
+            return true;
         }
     }
 
-    exitTime[u] = ++t;
-    s.pop(); // remove u da pilha
+    cycle.pop_back();
+    st[v] = false;
+    return false;
 }
