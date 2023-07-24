40b0c718f20933054dd0648f3b97128e8b653a66
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-07-24 12:59:41 -0300

    Update segtree lazy(sem classe).cpp

--- segtree/segtree lazy(sem classe).cpp
+++ segtree/segtree lazy(sem classe).cpp
@@ -1,37 +1,34 @@
-const int N = 1e5*2;
-int tree[4 * N];
-int lazy[4 * N];
+const int N = 2e5+5;
+int arr[N];
+ll tree[4 * N];
+ll lazy[4 * N];
 
-void push(int node, int l, int r) 
+void push(int node) 
 {
-    if (lazy[node]) 
-    {
-        tree[node] += (r - l + 1) * lazy[node];
-        if (l != r) 
-        {
-            lazy[2 * node] += lazy[node];
-            lazy[2 * node + 1] += lazy[node];
-        }
-        lazy[node] = 0;
-    }
+    tree[node*2] +=lazy[node];
+    tree[node*2+1]+=lazy[node];
+    lazy[node*2]+=lazy[node];
+    lazy[node*2+1]+=lazy[node];
+    lazy[node]=0;
 }
 
-void build(vector<int>& arr, int node, int l, int r) 
+void build(int node, int l, int r) //começar com node=1, l=0, r=n-1 ->isso segue para outros
 {
     if (l == r) 
     {
         tree[node] = arr[l];
-       return;
+        lazy[node]=0;
+        return;
     } 
-        int mid = (l + r) / 2;
-        build(arr, 2 * node, l, mid);
-        build(arr, 2 * node + 1, mid + 1, r);
-        tree[node] = tree[2 * node] + tree[2 * node + 1];
+	int mid = (l + r) / 2;
+	build( 2 * node, l, mid);
+	build( 2 * node + 1, mid + 1, r);
+	lazy[node]=0;
+    tree[node] = tree[2 * node] + tree[2 * node + 1];
 }
 
 void update(int node, int l, int r, int ql, int qr, int val) 
 {
-    push(node, l, r);
     if (qr < l || ql > r) 
     {
         return;
@@ -42,15 +39,24 @@ void update(int node, int l, int r, int ql, int qr, int val)
         push(node, l, r);
         return;
     } 
+    
+    push(node);
     int mid = (l + r) / 2;
-    update(2 * node, l, mid, ql, qr, val);
-    update(2 * node + 1, mid + 1, r, ql, qr, val);
+    
+    if (ql <= mid) 
+    {
+        update(2 * node, l, mid, ql, qr, val);
+    }
+    if (qr > mid) 
+    {
+        update(2 * node + 1, mid + 1, r, ql, qr, val);
+    }
+    
     tree[node] = tree[2 * node] + tree[2 * node + 1];
 }
 
-int query(int node, int l, int r, int ql, int qr) 
+ll query(int node, int l, int r, int ql, int qr) 
 {
-    push(node, l, r);
     if (qr < l || ql > r) 
     {
         return 0;
@@ -59,6 +65,8 @@ int query(int node, int l, int r, int ql, int qr)
     {
         return tree[node];
     } 
+    
+    push(node);
     int mid = (l + r) / 2;
     return query(2 * node, l, mid, ql, qr) + query(2 * node + 1, mid + 1, r, ql, qr);
 }
