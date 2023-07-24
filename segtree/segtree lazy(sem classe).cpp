29cecce8d38d6ca08a12ed8d0930c505eecf7dc4
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-07-24 11:03:21 -0300

    Update segtree lazy(sem classe).cpp

--- segtree/segtree lazy(sem classe).cpp
+++ segtree/segtree lazy(sem classe).cpp
@@ -21,15 +21,12 @@ void build(vector<int>& arr, int node, int l, int r)
     if (l == r) 
     {
         tree[node] = arr[l];
-       
+       return;
     } 
-    else 
-    {
         int mid = (l + r) / 2;
         build(arr, 2 * node, l, mid);
         build(arr, 2 * node + 1, mid + 1, r);
         tree[node] = tree[2 * node] + tree[2 * node + 1];
-    }
 }
 
 void update(int node, int l, int r, int ql, int qr, int val) 
@@ -39,18 +36,16 @@ void update(int node, int l, int r, int ql, int qr, int val)
     {
         return;
     } 
-    else if (ql <= l && qr >= r) 
+    if (ql <= l && qr >= r) 
     {
         lazy[node] += val;
         push(node, l, r);
+        return;
     } 
-    else 
-    {
-        int mid = (l + r) / 2;
-        update(2 * node, l, mid, ql, qr, val);
-        update(2 * node + 1, mid + 1, r, ql, qr, val);
-        tree[node] = tree[2 * node] + tree[2 * node + 1];
-    }
+    int mid = (l + r) / 2;
+    update(2 * node, l, mid, ql, qr, val);
+    update(2 * node + 1, mid + 1, r, ql, qr, val);
+    tree[node] = tree[2 * node] + tree[2 * node + 1];
 }
 
 int query(int node, int l, int r, int ql, int qr) 
@@ -60,13 +55,10 @@ int query(int node, int l, int r, int ql, int qr)
     {
         return 0;
     } 
-    else if (ql <= l && qr >= r) 
+    if (ql <= l && qr >= r) 
     {
         return tree[node];
     } 
-    else 
-    {
-        int mid = (l + r) / 2;
-        return query(2 * node, l, mid, ql, qr) + query(2 * node + 1, mid + 1, r, ql, qr);
-    }
+    int mid = (l + r) / 2;
+    return query(2 * node, l, mid, ql, qr) + query(2 * node + 1, mid + 1, r, ql, qr);
 }
