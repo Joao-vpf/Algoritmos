08c66fbb505fba7b9df418a61b1ccfc1f69bf9b8
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-08-04 13:27:16 -0300

    Atualizar o header.cpp

--- header.cpp
+++ header.cpp
@@ -23,41 +23,26 @@ Portin esteve aqui
 #define freop freopen("a.txt","r", stdin);freopen("11.txt","w", stdout); 
 #define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define MAX INT_MAX
+#define MIN INT_MIN
+#define INF 99999
+#define sz(x) (int(x.size()))
 #define unormap unordered_map 
-//#define sort(a) sort(a.begin(), a.end())
+#define CNS(i) const i&
 #define vecprnt(v, n) for(int i=0; i<n; i++) cout<<v[i] <<endl 
+#define FOR(i, n) for(; i<n; i++)
 #define upb upper_bound
 #define pii pair<int,int>
 using namespace std;
-typedef long int li;
-typedef long long ll;
-typedef unsigned long long ull;
-
+typedef int16_t li;
+typedef int32_t ll;
+typedef int64_t ill;//-2^63 a 2^63 - 1.
+typedef unsigned long long ull;//0 a 2^64 - 1.
+const ll MOD = 1e9 + 9;
+/*
 int fx[]={1,-1,0,0};
 int fy[]={0,0,1,-1};
-/*
+ 
 int fx[]={0,0,1,-1,-1,1,-1,1};
 int fy[]={-1,1,0,0,1,1,-1,-1};
 */
-
-int main()
-{
-	ios
-	//freop
-	
-}
-
-
-/*
-int main()
-{
-	int n = rand()%1000;
-	cout << n <<endl;
-	for(int i=0; i<n; i++)
-	{
-		cout << rand%100 << " ";
-	}
-	cout<<endl;
-}
-*/ 
-
+ 
