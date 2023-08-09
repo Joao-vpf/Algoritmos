c02c06829a7b48d224d053997bc30ef70b1d699f
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-08-09 11:59:36 -0300

    Update LCT O(log^2 N).cpp

--- Grafos/LCT O(log^2 N).cpp
+++ Grafos/LCT O(log^2 N).cpp
@@ -1,95 +1,159 @@
 
-struct Node 
-{
-  int fa, ch[2];
+struct Node
+{   int sz, label; /* size, label */
+    Node *p, *pp, *l, *r; /* parent, path-parent, left, right pointers */
+    Node() { p = pp = l = r = 0; }
 };
 
-class LinkCutTree 
-{
- public:
-  LinkCutTree(int n) : s(n + 1) 
-  {
-    for (int i = 1; i <= n; ++i) 
-    {
-      s[i].fa = s[i].ch[0] = s[i].ch[1] = 0;
+void update(Node *x)
+{   x->sz = 1;
+    if(x->l) x->sz += x->l->sz;
+    if(x->r) x->sz += x->r->sz;
+}
+
+void rotr(Node *x)
+{   Node *y, *z;
+    y = x->p, z = y->p;
+    if((y->l = x->r)) y->l->p = y;
+    x->r = y, y->p = x;
+    if((x->p = z))
+    {   if(y == z->l) z->l = x;
+        else z->r = x;
     }
-  }
+    x->pp = y->pp;
+    y->pp = 0;
+    update(y);
+}
 
-  void link(int u, int v) 
-  {
-    make_root(u);
-    s[u].ch[1] = v;
-    s[v].fa = u;
-  }
-
-  void cut(int u, int v) 
-  {
-    access(u);
-    make_root(v);
-    s[v].fa = s[u].ch[1] = 0;
-  }
-
-  int find_root(int u)
-  {
-    access(u);
-    while (s[u].ch[0]) 
-    {
-      u = s[u].ch[0];
+void rotl(Node *x)
+{   Node *y, *z;
+    y = x->p, z = y->p;
+    if((y->r = x->l)) y->r->p = y;
+    x->l = y, y->p = x;
+    if((x->p = z))
+    {   if(y == z->l) z->l = x;
+        else z->r = x;
     }
-    splay(u);
-    return u;
-  }
+    x->pp = y->pp;
+    y->pp = 0;
+    update(y);
+}
 
- private:
-  std::vector<Node> s;
-
-  void rotate(int x) 
-  {
-    int y = s[x].fa;
-    int z = s[y].fa;
-    int k = (s[y].ch[1] == x);
-    s[y].ch[k] = s[x].ch[1 - k];
-    s[s[x].ch[1 - k]].fa = y;
-    s[x].ch[1 - k] = y;
-    s[y].fa = x;
-    s[x].fa = z;
-    if (z) s[z].ch[s[z].ch[1] == y] = x;
-  }
-
-  void splay(int x) 
-  {
-    while (s[x].fa) 
-    {
-      int y = s[x].fa;
-      int z = s[y].fa;
-      if (z) {
-        if ((s[z].ch[0] == y) == (s[y].ch[0] == x)) rotate(y);
-        else rotate(x);
-      }
-      rotate(x);
+void splay(Node *x)
+{   Node *y, *z;
+    while(x->p)
+    {   y = x->p;
+        if(y->p == 0)
+        {   if(x == y->l) rotr(x);
+            else rotl(x);
+        }
+        else
+        {   z = y->p;
+            if(y == z->l)
+            {   if(x == y->l) rotr(y), rotr(x);
+                else rotl(x), rotr(x);
+            }
+            else
+            {   if(x == y->r) rotl(y), rotl(x);
+                else rotr(x), rotl(x);
+            }
+        }
     }
-  }
+    update(x);
+}
 
-  void access(int u) 
-  {
-    int v = 0;
-    while (u) 
-    {
-      splay(u);
-      s[u].ch[1] = v;
-      v = u;
-      u = s[u].fa;
+Node *access(Node *x)
+{   splay(x);
+    if(x->r)
+    {   x->r->pp = x;
+        x->r->p = 0;
+        x->r = 0;
+        update(x);
     }
-  }
 
-  void make_root(int u)
-  {
-    access(u);
-    splay(u);
-    if (s[u].ch[0]) 
-    {
-      s[s[u].ch[0]].fa = 0;
-      s[u].ch[0] = 0;
+    Node *last = x;
+    while(x->pp)
+    {   Node *y = x->pp;
+        last = y;
+        splay(y);
+        if(y->r)
+        {   y->r->pp = y;
+            y->r->p = 0;
+        }
+        y->r = x;
+        x->p = y;
+        x->pp = 0;
+        update(y);
+        splay(x);
     }
-  }
+    return last;
+}
+
+Node *root(Node *x)
+{   access(x);
+    while(x->l) x = x->l;
+    splay(x);
+    return x;
+}
+
+void cut(Node *x)
+{   access(x);
+    x->l->p = 0;
+    x->l = 0;
+    update(x);
+}
+
+void link(Node *x, Node *y)
+{   access(x);
+    access(y);
+    x->l = y;
+    y->p = x;
+    update(x);
+}
+
+Node *lca(Node *x, Node *y)
+{   access(x);
+    return access(y);
+}
+
+int depth(Node *x)
+{   access(x);
+    return x->sz - 1;
+}
+
+class LinkCut
+{   Node *x;
+
+    public:
+    LinkCut(int n)
+    {   x = new Node[n];
+        for(int i = 0; i < n; i++)
+        {   x[i].label = i;
+            update(&x[i]);
+        }
+    }
+
+    virtual ~LinkCut()
+    {   delete[] x;
+    }
+
+    void link(int u, int v)
+    {   ::link(&x[u], &x[v]);
+    }
+
+    void cut(int u)
+    {   ::cut(&x[u]);
+    }
+
+    int root(int u)
+    {   return ::root(&x[u])->label;
+    }
+
+    int depth(int u)
+    {   return ::depth(&x[u]);
+    }
+
+    int lca(int u, int v)
+    {   return ::lca(&x[u], &x[v])->label;
+    }
 };
