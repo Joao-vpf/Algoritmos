5a24d0ed2ed56ee9bc1dadaaa6c8ccbc4529e8a6
João Victor Porto Fernandes <98399932+Joao-vpf@users.noreply.github.com>
2023-08-25 20:02:45 -0300

    Update header.cpp

--- header.cpp
+++ header.cpp
@@ -13,6 +13,9 @@ Portin esteve aqui
  
 */
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
 #define pb push_back
 #define pf push_front
 #define ppb pop_back
@@ -23,21 +26,32 @@ Portin esteve aqui
 #define freop freopen("a.txt","r", stdin);freopen("11.txt","w", stdout); 
 #define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define MAX INT_MAX
+#define MAXX LONG_LONG_MAX
+#define MINN LONG_LONG_MIN	
 #define MIN INT_MIN
-#define INF 99999
-#define sz(x) (int(x.size()))
+#define INF 1e9
+#define prq priority_queue
 #define unormap unordered_map 
 #define CNS(i) const i&
-#define vecprnt(v, n) for(int i=0; i<n; i++) cout<<v[i] <<endl 
-#define FOR(i, n) for(; i<n; i++)
+#define sz(x) int(x.size())
+#define vecprnt(v, n) for(int i=0; i<n; i++) cout<<v[i] <<" " 
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
 #define upb upper_bound
+#define lwb lower_bound
+#define memo(x, o) memset(x, o, sizeof(x))
+#define mems(x) memset(x, 0, sizeof(x)) 
 #define pii pair<int,int>
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
 using namespace std;
-typedef int16_t li;
-typedef int32_t ll;
-typedef int64_t ill;//-2^63 a 2^63 - 1.
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+typedef int_fast16_t li;
+typedef int_fast32_t ll;
+typedef int_fast64_t ill;//-2^63 a 2^63 - 1.
 typedef unsigned long long ull;//0 a 2^64 - 1.
 const ll MOD = 1e9 + 9;
+const int MAXN = 1e5+10;
 /*
 int fx[]={1,-1,0,0};
 int fy[]={0,0,1,-1};
@@ -45,27 +59,28 @@ int fy[]={0,0,1,-1};
 int fx[]={0,0,1,-1,-1,1,-1,1};
 int fy[]={-1,1,0,0,1,1,-1,-1};
 */
-
-
- 
 int main()
 {
 	ios
-	//freop
+	//freop 
 }
- 
- 
-/*
-int main()
-{
-	int n = rand()%1000;
-	cout << n <<endl;
-	for(int i=0; i<n; i++)
-	{
-		cout << rand%100 << " ";
-	}
-	cout<<endl;
-}
-*/ 
 
+//int main()
+//{
+	//ios
+	//freop
+	//srand(time(0));
+	//for(int i=0; i<10; i++)
+	//{
+	//int n = rand()%200+1;
+	//cout << n << " " << rand()%30+1 <<endl;
+	//set<int> st;
+	//while(st.size()<n) st.insert(rand()%1000);
+	//for(auto e : st)
+	//{
+		//cout << e << endl;
+	//}
+	//cout<<endl;
+	//}
+//}
  
